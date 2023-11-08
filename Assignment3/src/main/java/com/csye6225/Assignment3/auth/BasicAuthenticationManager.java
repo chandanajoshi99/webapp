a3c83f72064da24a210aa2d2a5a0267a43b9b318
joshichandana <joshi.chandana412@gmail.com>
2023-11-08 11:59:33 -0500

    Commit Changes for Assignment-7 (#45)

--- Assignment3/src/main/java/com/csye6225/Assignment3/auth/BasicAuthenticationManager.java
+++ Assignment3/src/main/java/com/csye6225/Assignment3/auth/BasicAuthenticationManager.java
@@ -1,5 +1,6 @@
 package com.csye6225.Assignment3.auth;
 
+import com.csye6225.Assignment3.controller.AssignmentController;
 import com.csye6225.Assignment3.repository.UserRepository;
 import com.csye6225.Assignment3.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -19,6 +22,8 @@ import java.util.Optional;
 
 @Component
 public class BasicAuthenticationManager implements AuthenticationManager {
+
+    private static final Logger logger = LoggerFactory.getLogger(AssignmentController.class);
     @Autowired
     private UserRepository userRepository;
     @Autowired
@@ -28,20 +33,17 @@ public class BasicAuthenticationManager implements AuthenticationManager {
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getPrincipal() + "";
         String password = authentication.getCredentials() + "";
-        System.out.println("In AuthenticateManager " );
-        System.out.println("Username " + username + " Pass " + password);
+//        System.out.println("In AuthenticateManager " );
+//        System.out.println("Username " + username + " Pass " + password);
         Optional<User> user =  userRepository.findByEmail(username);
         if (user == null) {
+            logger.error("User Doesn't exist");
             throw new BadCredentialsException("1000");
         }
         if (!passwordEncoder.matches(password, user.get().getPassword())) {
+            logger.error("Password doesn't match");
             throw new BadCredentialsException("1000");
         }
-//        if (user.get().isDisabled()) {
-//            throw new DisabledException("1001");
-//        }
-//        List<Right> userRights = rightRepo.getUserRights(username);
-//        List<SimpleGrantedAuthority> grantedAuthorities = new ArrayList<>();
         SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER");
         List<GrantedAuthority> list = new ArrayList<>();
         list.add(authority);
